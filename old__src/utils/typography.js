d8a2b75de1e22a97dd2f10c4f4453bc52a5e6638
kwpkielce <kwp@tlen.pl>
2020-12-28 12:19:02 +0100

    28

--- old__src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography"
-import kirhamTheme from "typography-theme-kirkham"
-
-const typography = new Typography(kirhamTheme)
-
-export default typography
-export const rhythm = typography.rhythm
